5d281cba8fc0704fb6fa6e0257d8213c89d354cc
wake770 <2771601060@qq.com>
2023-06-09 14:34:59 +0800

    home-整体结构拆分以及分类的数据渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,4 +1,18 @@
+<script setup>
+
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
+</script>
+
 <template>
-    我是home页面
-    <div style="height: 1500px;"></div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
 </template>
