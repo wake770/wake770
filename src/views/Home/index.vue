845f0a4e37ffe7fafa0b89d75b8c1a76765df406
wake770 <2771601060@qq.com>
2023-06-09 11:12:23 +0800

    吸顶导航交互实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,4 @@
 <template>
-    我是Home页面
+    我是home页面
+    <div style="height: 1500px;"></div>
 </template>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,6 +3,7 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue';
 </script>
 
 <template>
@@ -10,4 +11,5 @@ import LayoutFooter from './components/LayoutFooter.vue'
   <LayoutHeader />
   <RouterView />
   <LayoutFooter />
+  <LayoutFixed />
 </template>
