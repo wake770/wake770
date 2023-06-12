72536005ff13f68a97c390997c0cccf46c7bee5e
wake770 <2771601060@qq.com>
2023-06-12 16:11:43 +0800

    home-轮播图实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,6 +2,7 @@
 import{ getCategoryAPI } from '@/apis/category'
 import { useRoute } from 'vue-router'
 import { onMounted, ref } from 'vue'
+import { getBannerAPI } from '@/apis/home'
 //获取数据
 const categoryData = ref({})
 const route = useRoute()
@@ -10,6 +11,18 @@ const getCategory = async () => {
     categoryData.value = res.result
 }
 onMounted(() => getCategory())
+//获取banner
+const bannerList = ref([])
+
+const getBanner = async() => {
+    const res = await getBannerAPI({
+        distributionSite: '2'
+    })
+    console.log(res)
+    bannerList.value = res.result
+}
+
+onMounted(() => getBanner())
 </script>
 
 <template>
@@ -22,12 +35,34 @@ onMounted(() => getCategory())
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+            <el-carousel-item v-for="item in bannerList" :key="item.id">
+                <img :src="item.imgUrl" alt="">
+            </el-carousel-item>
+        </el-carousel>
+    </div>
     </div>
   </div>
 </template>
 
 
 <style scoped lang="scss">
+.home-banner {
+    width: 1240px;
+    height: 500px;
+    // position: absolute;
+    // left: 0;
+    // top: 0;
+    margin: 0 auto;
+    z-index: 98;
+
+    img {
+        width: 100%;
+        height: 500px;
+    }
+}
 .top-category {
   h3 {
     font-size: 28px;
