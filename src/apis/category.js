11107a3cbbfbe67b7ebcae9447a1c65ff0cdefc4
wake770 <2771601060@qq.com>
2023-06-13 09:55:59 +0800

    二级分类-面包屑导航

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,9 +1,24 @@
 import request from '@/utils/http'
 
 
-export function getCategoryAPI (id) {
+export function getCategoryAPI(id) {
     return request({
         url: '/category',
+        params: {
+            id
+        }
+    })
+}
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 分类id 
+ * @return {*}
+ */
+
+export const getCategoryFilterAPI = (id) => {
+    return request({
+        url: '/category/sub/filter',
         params: {
             id
         }
