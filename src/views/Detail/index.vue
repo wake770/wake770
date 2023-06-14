6c0a9da9d6d4902c861e197a69afbcf295419f88
wake770 <2771601060@qq.com>
2023-06-14 10:11:23 +0800

    通用组件统一注册全局

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,10 +1,10 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue'
 import { getDetail } from '@/apis/detail'
-import XtxSku from '@/components/XtxSku/index.vue'
+// import XtxSku from '@/components/XtxSku/index.vue'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
-import ImageView from '@/components/ImageView/index.vue'
+// import ImageView from '@/components/ImageView/index.vue'
 
 const goods = ref({})
 const route = useRoute()
@@ -44,7 +44,7 @@ const skuChange = (sku) => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView :image-list="goods.mainPictures"/>
+              <XtxImageView :image-list="goods.mainPictures"/>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
