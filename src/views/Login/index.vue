f1b704e767daee332cdead8423534865f26b58fa
wake770 <2771601060@qq.com>
2023-06-16 16:09:50 +0800

    登录-表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,6 +1,23 @@
 <script setup>
 
-
+//表单检验（账户名+密码）
+import { trigger } from '@vue/reactivity';
+import { ref } from 'vue'
+// 1.准备表单对象
+const form = ref({
+  account: '',
+  password: ''
+})
+// 2.准备规则对象
+const rules = {
+  account:[
+    {required:true,message:'用户名不能为空',trigger:'blur'}
+  ],
+  password:[
+    { required:true,message:'密码不能为空',trigger:'blur' },
+    { min:6,max:14,message:'密码长度为6-14字符',trigger:'blur' }
+  ]
+}
 </script>
 
 
@@ -25,14 +42,14 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
+            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
-              </el-form-item>
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" />
+              </el-form-item >
               <el-form-item label-width="22px">
                 <el-checkbox  size="large">
                   我已同意隐私条款和服务条款
@@ -40,7 +57,7 @@
               </el-form-item>
               <el-button size="large" class="subBtn">点击登录</el-button>
             </el-form>
-          </div>
+          </div> 
         </div>
       </div>
     </section>
