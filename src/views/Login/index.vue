9299a56429424e74283b859b684018ae4ec60078
wake770 <2771601060@qq.com>
2023-06-16 16:34:10 +0800

    登录-表单校验-统一校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -32,6 +32,15 @@ const rules = {
     }
   ]
 }
+
+//3.获取form实例做同意校验
+const formRef = ref(null)
+const doLogin = () =>{
+  formRef.value.validate((valid) => {
+    //valid:所有表单都通过检验 才为true
+    console.log(valid)
+  })
+}
 </script>
 
 
@@ -56,7 +65,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
               <el-form-item prop="account" label="账户">
                 <el-input v-model="form.account" />
@@ -69,7 +78,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin" >点击登录</el-button>
             </el-form>
           </div> 
         </div>
